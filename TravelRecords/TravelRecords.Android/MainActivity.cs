c599412242e7b406d1d79ada6a857f2a9e620f0a
Mario Oliveira <mario.oliv96@outlook.com>
2018-06-01 16:12:21 +0100

    Maps and user's location added

--- TravelRecords/TravelRecords.Android/MainActivity.cs
+++ TravelRecords/TravelRecords.Android/MainActivity.cs
@@ -7,25 +7,37 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using System.IO;
+using Plugin.CurrentActivity;
+using Plugin.Permissions;
 
 namespace TravelRecords.Droid
 {
+
     [Activity(Label = "TravelRecords", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            //CrossCurrentActivity.Current.Init(this, bundle);
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            CrossCurrentActivity.Current.Init(this, bundle);
+            Xamarin.FormsMaps.Init(this, bundle);
 
             string dbName = "travel_db.sqlite";
             string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
             string fullPath = Path.Combine(folderPath, dbName);
             LoadApplication(new App(fullPath));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
--- TravelRecords/TravelRecords/MapPage.xaml.cs
+++ TravelRecords/TravelRecords/MapPage.xaml.cs
@@ -1,20 +1,93 @@
-﻿using System;
+﻿using Plugin.Geolocator;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace TravelRecords
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MapPage : ContentPage
-	{
-		public MapPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    public partial class MapPage : ContentPage
+    {
+        private Map map = new Map(
+                        MapSpan.FromCenterAndRadius(
+                                new Position(37, -122), Distance.FromMiles(0.3)))
+        {
+            HeightRequest = 100,
+            WidthRequest = 960,
+            VerticalOptions = LayoutOptions.FillAndExpand
+        };
+
+        private bool permissionRequested = false;
+
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Plugin.Permissions.Abstractions.Permission.Location);
+                if (status != PermissionStatus.Granted && !permissionRequested)
+                {
+
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
+                    {
+                        await DisplayAlert("Need permission", "App will have access to your location", "Ok");
+                    }
+
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Location);
+
+                    if (results.ContainsKey(Permission.Location))
+                    {
+                        status = results[Permission.Location];
+                    }
+                    permissionRequested = true;
+                }
+                if (status == PermissionStatus.Granted )
+                {
+                    map.IsShowingUser = true;
+                  
+                }
+                else if (status != PermissionStatus.Granted && permissionRequested)
+                {
+                    //await DisplayAlert("No permission", "You didnt grant permission to access your location", "Ok");
+                    map.IsShowingUser = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+             
+            var locator = CrossGeolocator.Current;
+            locator.PositionChanged += Locator_PositionChanged;
+            TimeSpan timeSpan = new TimeSpan(0);
+            await locator.StartListeningAsync(timeSpan, 10);
+
+
+            var position = await locator.GetPositionAsync();
+            var centre = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
+            var span = new Xamarin.Forms.Maps.MapSpan(centre, 2, 2);
+            locationMap.MoveToRegion(span);
+            
+        }
+
+        private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            var centre = new Xamarin.Forms.Maps.Position(e.Position.Latitude, e.Position.Longitude);
+            var span = new Xamarin.Forms.Maps.MapSpan(centre, 2, 2);
+            locationMap.MoveToRegion(span);
+        }
+    }
 }
